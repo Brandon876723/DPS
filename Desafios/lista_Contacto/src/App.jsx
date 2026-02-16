28fde91eaac727084105f790d1509da3e6e7ba0e
MeylinT28 <jazminfuentes339@gmail.com>
2026-02-16 12:37:07 -0600

    cambios en el App.jsx

--- Desafios/lista_Contacto/src/App.jsx
+++ Desafios/lista_Contacto/src/App.jsx
@@ -1,7 +1,13 @@
-function App() {
-   return ( 
-   <div className="App">
+import { useState, useEffect } from "react";
+import ContactList from "./componentes/contactList";
+import contactsData from "./contactos.json";
+import "./App.css";
+
+function App() { 
+  return (
+     <div className="App"> 
      <h1>📒 Agenda de Contactos</h1> 
-     <ContactList /> 
-     </div> );
- }
+     <ContactList /> </div> ); 
+     }
+
+ export default App;
